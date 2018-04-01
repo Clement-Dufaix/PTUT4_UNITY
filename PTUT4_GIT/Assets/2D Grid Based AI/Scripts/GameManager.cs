daf87632c2d7c53f81dfd4617062945da505f1a4
zdpowerz <33456786+zdpowerz@users.noreply.github.com>
2018-04-01 17:42:36 +0200

    Ajout de la tour

--- PTUT4_GIT/Assets/2D Grid Based AI/Scripts/GameManager.cs
+++ PTUT4_GIT/Assets/2D Grid Based AI/Scripts/GameManager.cs
@@ -216,8 +216,10 @@ public class GameManager : MonoBehaviour
     {
         if (grid[x, y].IsBlockable != true)
         {
-            //cases[x,y].GetComponent<SpriteRenderer>().sprite = towerPrefab;
-            cases[x, y].GetComponent<Renderer>().material.color = Color.red;
+            Sprite nb = (Sprite)Sprite.Instantiate(TowerPrefab);
+            
+            cases[x, y].GetComponent<SpriteRenderer>().sprite= nb;
+            cases[x, y].GetComponent<Renderer>().material.color = Color.black;
             grid[x, y].IsWall = true;
         }
 
