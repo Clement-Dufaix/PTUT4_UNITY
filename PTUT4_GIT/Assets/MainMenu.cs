6c3093a6011601493025da71d4a21107abc8847a
clement413@gmail.com <clement413@gmail.com>
2018-04-02 16:12:53 +0200

    addMenu
    addMenu

---
+++ PTUT4_GIT/Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start_the_game () {
+        Application.LoadLevel("Main");
+	}
+
+    // Update is called once per frame
+    public void Exit_the_game()
+    {
+        Application.Quit();
+    }
+}
