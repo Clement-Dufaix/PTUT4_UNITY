522e51ce61c4c93b73ce872fbb83206fc72ca15f
zdpowerz <33456786+zdpowerz@users.noreply.github.com>
2018-04-02 22:23:49 +0200

    Ajout d'un menu GAME OVER
    Et changement de fond car le font vert me cassais les couilles( trop moche)

---
+++ PTUT4_GIT/Assets/2D Grid Based AI/Scripts/playerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Contrôleur du joueur
+/// </summary>
+public class playerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - La vitesse de déplacement
+    /// </summary>
+    
+
+    void Update()
+    {
+        // 3 - Récupérer les informations du clavier/manette
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+    }
+    void OnDestroy()
+    {
+        // Game Over.
+        var gameOver = FindObjectOfType<GameOverScript>();
+        gameOver.ShowButtons();
+    }
+
+    void FixedUpdate()
+    {
+     
+    }
+
+
+}
